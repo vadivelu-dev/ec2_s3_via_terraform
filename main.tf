6139c71152be241063a7d817162876f8d124de44
Vadivelu M <vadivelumsolo@gmail.com>
2026-06-16 21:09:37 +0530

    push

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "ec2_via_terraform" {
+  ami           =  "ami-0b6d9d3d33ba97d99"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
+
+resource "aws_s3_bucket" "s3_via_terraform" {
+  bucket = "s3-via-terraform-bucket_2026"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
